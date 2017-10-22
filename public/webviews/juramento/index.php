4a583595cade8899c824669807bd789535ec1dbb
Mario Salas <mario.salas@gmail.com>
2017-10-21 22:16:28 -0500

    Cambio Juramento

--- public/webviews/juramento/index.php
+++ public/webviews/juramento/index.php
@@ -6,19 +6,13 @@
 
 <style>
 
-#logo{
-
-width: 400px;
-height: auto;
-
-x}
 
 #texto{
 
 text-align: justify;
 text-align: center;
 font-family: sans-serif;
-font-size: 25px;
+font-size: 20px;
 
 
 }
@@ -28,7 +22,7 @@ font-size: 25px;
 </head>
 <body bgcolor= "#FFD900" bgwidth:"2px" height:"20px">
 <center> 
-<img id="logo" src="images\bsc.png">
+
  
 <?php
 $texto = "Juro ser amarillo de nacimiento y pertenecer <br> de corazón al único ídolo del Ecuador... <br> Bacerlona Sporting Club <br> Si es así has clic en Aceptar";
