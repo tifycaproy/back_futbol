f24ad488ae68aa2d9bf45012cc2961cccfb0ae61
Mario Salas <mario.salas@gmail.com>
2017-10-24 00:28:07 -0500

    Livestreaming

--- public/webviews/livestream/index.php
+++ public/webviews/livestream/index.php
@@ -1,37 +1,114 @@
 <!DOCTYPE html>
 <html>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<head>
-  <title>Livestream</title>
-
+  <head>
+    <meta charset="utf-8">
+    <title>Nick Pettit</title>
+    
+     <script src="dist/js/lightbox-plus-jquery.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 <style>
-
-#texto{
-
-text-align: justify;
-text-align: center;
-font-family: sans-serif;
-font-size: 35px;
-
-
+li {
+	list-style: none;
+	list-style-image: none;
+	padding-bottom: 20px;
 }
+
+ul {
+	padding: 0px;
+	margin: 0px;
+}
+body {
+	background: #000;
+	/* margin: auto; */
+	/* position: relative; */
+	/* min-width: 900px; */
+	/* display: flex; */
+	padding: 0px;
+	margin: 0px;
+}
+.center {
+	margin: auto;
+	min-width: 100px;
+	text-align: center;
+}
+h2{
+	font-size: 18px;
+	color: #fff;
+	font-family: 'Open Sans', sans-serif;
+	text-align: left;
+margin: 0px;
+}
+img {
+	width: 100%;
+	height: auto;
+}
+a:link   
+{   
+ text-decoration:none;   
+}   
 
 </style>
 
-</head>
-<body bgcolor= "#E9EBEB" bgwidth:"2px" height:"20px">
-<center> 
-<?php
-$texto = "Livestream2";
+  </head>
+  <body>
+   
+<div class="center">
 
-  echo "<br>";
-  echo "<div id=\"texto\">";
-  echo $texto;
-  echo "</div>";
-  
-  
-?> 
-</center>
-</div>  
+<ul>
+     
+     
+    <li>
+      <a class="example-image-link" href="imagenes/1.jpg" data-lightbox="example-1">
+       <img class="example-image" src="imagenes/1.jpg" alt="image-1" />
+            <h2>No se pierden las ganas de alentarte</h2>
+       </a>
+    </li>
+    <li>
+      <a class="example-image-link" href="imagenes/2.jpg" data-lightbox="example-1">
+       <img class="example-image" src="imagenes/2.jpg" alt="image-1" />
+            <h2>Creo en ti </h2>
+       </a>
+    </li>
+    <li>
+      <a class="example-image-link" href="imagenes/3.jpg" data-lightbox="example-1">
+       <img class="example-image" src="imagenes/3.jpg" alt="image-1" />
+            <h2>Los clásicos no se juegan, se ganan</h2>
+       </a>
+    </li>
+    <li>
+      <a class="example-image-link" href="imagenes/4.jpg" data-lightbox="example-1">
+       <img class="example-image" src="imagenes/4.jpg" alt="image-1" />
+            <h2>92 años de gloria Amarilla</h2>
+       </a>
+    </li>
+    <li>
+      <a class="example-image-link" href="imagenes/5.jpg" data-lightbox="example-1">
+       <img class="example-image" src="imagenes/5.jpg" alt="image-1" />
+            <h2>Viernes de Revancha</h2>
+       </a>
+    </li>
+    <li>
+      <a class="example-image-link" href="imagenes/6.jpg" data-lightbox="example-1">
+       <img class="example-image" src="imagenes/6.jpg" alt="image-1" />
+            <h2>El domingo volvemos al monumental</h2>
+       </a>
+    </li>
+    <li>
+      <a class="example-image-link" href="imagenes/7.jpg" data-lightbox="example-1">
+       <img class="example-image" src="imagenes/7.jpg" alt="image-1" />
+            <h2>El Monumental siempre contigo</h2>
+       </a>
+    </li>
+    
+</ul>
+ 
+    </div>
+       <link rel="stylesheet" href="dist/css/lightbox.min.css">
   </body>
+  
+  
 </html>
+<?php
+
+
+?>
