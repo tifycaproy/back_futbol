c10937e053ee3368b2827d5f2b2618cdc7476ee4
Mario Salas <mario.salas@gmail.com>
2017-10-24 15:19:28 -0500

    livestreaming pruebas

--- public/webviews/livestream/index.php
+++ public/webviews/livestream/index.php
@@ -1,3 +1,1 @@
-<div style="width:100%;height:100%;width: 820px; height: 461.25px; float: none; clear: both; margin: 2px auto;">
-  <embed src="http://www.youtube.com/v/GlIzuTQGgzs?version=3&amp;hl=en_US&amp;rel=0&amp;autohide=1&amp;autoplay=1" wmode="transparent" type="application/x-shockwave-flash" width="100%" height="100%" allowfullscreen="true" title="Adobe Flash Player">
-</div>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/oPpEio5Fz2w" frameborder="0" allowfullscreen></iframe>
