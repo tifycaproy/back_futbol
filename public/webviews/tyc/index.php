6c526f58b3b8b42ddb20916f8cb5d93f14a4409d
gbanchs <gabanchsra31@hotmail.com>
2017-11-20 12:10:28 -0400

    Agreagando terminos y condiciones al webview tyc

--- public/webviews/tyc/index.php
+++ public/webviews/tyc/index.php
@@ -1,8 +1,163 @@
-<?php
+<!doctype html>
+<html lang="es">
+<head>
+ <meta name="viewport" content="width=device-width, initial-scale=1" user-scalable="no">
+<style>
+</style>
+<body>
+<p>
+TÉRMINOS Y CONDICIONES DE ACCESO Y USO DE LA APLICACIÓN OFICIAL DE AZUL & BLANCO MILLONARIOS F.C S.A
 
+El presente documento establece los términos y las condiciones por medio de los cuales se regirá el acceso y uso de la aplicación AZUL & BLANCO MILLONARIOS F.C S.A, en adelante LA APLICACIÓN, la cual es propiedad de AZUL & BLANCO MILLONARIOS F.C S.A Y 2WAYSPORTS, en adelante LOS PROPIETARIOS.
+
+La APLICACIÓN está disponible para descarga gratuita en iOS y Android en todos los mercados disponibles a nivel mundial.
+
+El usuario se obliga a leer los términos y condiciones aquí establecidos, antes de efectuar la descarga de LA APLICACIÓN, en consecuencia, al efectuar la instalación si así lo decide, se entenderá que conoce, acepta y se somete a lo establecido en los presentes Términos y Condiciones.
+
+1. COMPROMISOS Y CONDUCTA DE LOS USUARIOS.
+
+Los usuarios de LA APLICACIÓN se comprometen a:
+
+A. Suministrar información veraz y correcta
+
+B. El usuario se declara libre y responsable sobre la información personal que ingrese a LA APLICACIÓN, liberando de toda responsabilidad a LOS PROPIETARIOS por cualquier inexactitud o falta de veracidad sobre dicha información
+
+C. El usuario se compromete a usar LA APLICACIÓN y sus contenidos de forma legal, precisa y bajo las normas de orden público, moral y buenas costumbres.
+
+D. Abstenerse de reproducir, alterar, compartir, copiar, representar, simbolizar, usar, distribuir, transformar, transmutar, convertir o modificar los contenidos de LA APLICACIÓN, por cualquier procedimiento o sobre cualquier soporte conocido o por conocerse, de forma total o parcial
+
+E. Abstenerse de usar LA APLICACIÓN o sus contenidos de forma que causen un riesgo, daño o inutilización de LA APLICACIÓN o de sus contenidos
+
+F. Abstenerse de alterar, destruir, suprimir, esquivar o manipular el derecho de autor o los datos del autor de LA APLICACIÓN o de sus contenidos, o de los mecanismos técnicos y tecnológicos de seguridad y privacidad de la información
+
+G. Abstenerse de usar LA APLICACIÓN o sus contenidos con una finalidad de comercialización o de publicidad para beneficio del usuario o de terceros
+
+H. Abstenerse de usar LA APLICACIÓN y sus contenidos con propósitos legales o ilegales, en controversia a lo estipulado en estos Términos y Condiciones, o al uso mismo de LA APLICACIÓN, que sean contrarios a los derechos e intereses de terceros, o usar de cualquier forma LA APLICACIÓN y que con dicho
+
+uso puedan invalidar, deteriorar, recargar o malograr LA APLICACIÓN y los contenidos u obstaculizar su uso normal y justo para los demás usuarios
+
+I. Tomar medidas y maneras adecuadas, comportarse diligentemente en el momento de usar a LA APLICACIÓN y contar con herramientas de protección, antivirus, para manejo de malware, spyware y similares
+
+J. Los servicios y contenidos que se prestan en LA APLICACIÓN se podrán utilizar solamente para propósitos legales, entendiéndose que cualquier tipo de conducta o actividad que pueda calificarse como ilegal conforme a las reglas internacionales y colombianas, así como a buenas practicas estará de plano prohibida, y será de exclusivo cargo del usuario la asunción de cualquier tipo de responsabilidad a él imputable, así como de la aplicación de sanciones legales a que haya lugar
+
+K. Abstenerse de desplegar conductas ilegales que comprenden entre otras actividades las siguientes: realizar, promover o permitir la publicación o transmisión de cualquier tipo de información o material ilegal, ilícita, amenazante, abusivo, difamatorio, obsceno, ofensivo, pornográfico, indecente, profano, o información o de alguna u otra manera desagradable, incluyendo sin la limitación ningunas transmisiones que constituyen o que animan la conducta que constituiría una conducta penalizada por la Ley.
+
+2. USOS Y SERVICIOS DE LA APLICACIÓN.
+
+· Creación de Perfiles: los usuarios podrán registrarse como Hinchas Emajadores Oficiales y crear su perfil para lo cual deberán llenar el registro con su nombre, apellido, email, fotografía, teléfono, país de residencia y fecha de nacimiento con lo cual podrán obtener su credencial de Hincha Embajador Oficial
+
+· Noticias: los usuarios tendrán acceso por medio de LA APLICACIÓN a las noticias de azul & blanco millonarios f.c s.a, información de sus jugadores y su desempeño tanto con el equipo como con sus clubes, calendario de partidos y tabla de posiciones y demás información y eventos noticiosos relacionados a su nombre
+
+· Equipo: los usuarios podrán conocer la trayectoria y aspectos deportivos de los jugadores convocados a torneos y partidos, así como también podrán aplaudir a sus jugadores favoritos, para valorar su desempeño
+
+· Alineación: los usuarios podrán tener acceso exclusivo a la alineación de los partidos, así como ver el ranking de los jugadores más aplaudidos por partido y acumulado
+
+· Realidad Virtual: los usuarios podrán acceder a videos en realidad virtual de los partidos, entrenamientos y momentos de los jugadores tras bastidores
+
+· Fútbol base: los usuarios podrán tener acceso en esta sección a la información, noticias, y en general todo lo relacionado con las divisiones menores de azul & blanco millonarios f.c S.A
+
+· Calendario: los usuarios podrán encontrar las fechas que ha disputado y que disputara el equipo.
+
+· Academia: En esta sección los usuarios podran acceder a la pagina oficial de la academia de de azul & blanco millonarios f.c S.A, para registrarse en la academia.
+
+· Beneficios Hincha: los usuarios encontrarán todos los beneficios a los cuales los hinchas de azul & blanco millonarios f.c S.A podrán acceder con presentar su credencial oficial.
+
+· Estadisticas: En esta sección podrán encontrar todas las estadisticas de los partidos, y el rendimiento del equipo.
+
+· Tienda virtual: Los usuarios pódran acceder y comprar los productos oficiales que se encuentran en a la tienda virtual ofcial de de azul & blanco millonarios f.c S.A
+
+3. EXCLUSIÓN DE RESPONSABILIDAD, MODIFICACIONES, DENEGACIÓN Y RETIRADA DE LA APLICACIÓN: LOS PROPIETARIOS podrán en cualquier momento, efectuar modificaciones en LA APLICACIÓN, así como de suspender el acceso en cualquier momento y sin previo aviso. LOS PROPIETARIOS no garantizan la utilidad de LA APLICACIÓN para la realización de las actividades o funcionalidades descritas en los presentes Términos y Condiciones, ni su infalibilidad y en consecuencia no garantiza la operación permanente y libre de fallas de LA APLICACIÓN, ni asume responsabilidad por los daños sufridos al Usuario por el no uso o imposibilidad de uso de la información o servicios prestados a través de LA APLICACIÓN. Así mismo, no garantiza que los contenidos y servicios contenidos en LA APLICACIÓN se encuentren operando o disponible en todo momento, ni que se encuentren actualizados y sean fiables en su totalidad.
+
+LOS PROPIETARIOS no serán responsables por la pérdida de información de los usuarios por, fuerza mayor o caso fortuito, por la pérdida, extravío o robo del dispositivo móvil que suponga el acceso de terceros a la aplicación móvil, por errores en el uso, digitación o accesos por parte del usuario a LA APLICACIÓN, por perjuicios, lucro cesante, daño emergente, daño moral, retrasos, no procesamiento de información o suspensión del servicio del operador móvil o daños en los dispositivos móviles. Asimismo,no serán responsables por interferencias no permitidas de terceros a las bases de datos de APP que puedan afectar, recolectar o modificar datos que se encuentras dentro de ésta misma.
+
+4. PROPIEDAD INTELECTUAL
+
+El usuario queda obligado a usar LA APLICACIÓN de forma correcta y lícita y de acuerdo con lo regulado por el régimen legal sobre los Derechos de Autor y Propiedad Industrial.
+
+La totalidad de las imágenes, marcas comerciales, logotipos, archivos de audio y vídeo, archivos de software, combinaciones de colores, así como la estructura, selección, ordenación y presentación de sus contenidos, material informático, gráfico, publicitario, fotográfico, de multimedia, audiovisual y de diseño, así como todos los contenidos, textos y bases de datos son propiedad de LOS PROPIETARIOS, los cuales se encuentran protegidos por las leyes Colombianas de la Comunidad Andina e internacionales sobre derechos de autor y propiedad industrial.
+
+En ningún caso, el acceso a los contenidos implica ningún tipo de renuncia, transmisión, licencia o cesión total ni parcial de dichos derechos. Los Términos y Condiciones acá expresados no confieren a los usuarios ningún otro derecho de utilización, alteración, explotación, reproducción, distribución o comunicación pública de la información contenida en LA APLICACIÓN.
+
+Los usuarios podrán utilizar y visualizar los contenidos dispuestos en LA APLICACIÓN, exclusivamente para su uso personal, privado y no lucrativo.
+
+Los enlaces a otros sitios web, LOS PROPIETARIOS no se hacen responsables de los contenidos y servicios de dichos sitios, ni tampoco comparte necesariamente las opiniones que en ellas se manifiesten.
+
+El usuario que considere que una obra o creación intelectual ha sido divulgada en LA APLICACIÓN, con violación a normas legales o convenios internacionales referentes a propiedad industrial o derecho de autor y conexos, puede notificar esta situación al siguiente correo electrónico: appmillonarios@2waysports.com
+
+Así mismo, el uso en LA APLICACIÓN de algunos materiales de propiedad de terceros se encuentra expresamente autorizado por la ley o por estos terceros.
+
+Queda prohibido todo acto referente a la disposición de los derechos morales y patrimoniales sobre las obras o contenidos dispuestos en LA APLICACIÓN por ello, entre otras actividades queda prohibido todo acto de copia, reproducción, modificación, creación de trabajos derivados, venta o distribución, exhibición de los contenidos de esta aplicación, por todos los medios conocidos y por conocerse, incluyendo, más no limitado a, medios electrónicos, mecánicos, de fotocopiado, de grabación, etc., sin el permiso previo, expreso y por escrito de LOS PROPIETARIOS.
+
+5. TRATAMIENTO DE LOS DATOS PERSONALES
+
+El responsable del tratamiento de los datos personales es azul & blanco millonarios f.c S.A.
+
+Sólo se almacenarán los datos personales que el usuario voluntariamente haya facilitado al acceder o inscribirse en alguno de los servicios, juegos y demás dispuestos en LA APLICACIÓN. Los datos de carácter personal que el usuario ingrese en LA APLICACIÓN, son directamente recogidos, tratados y controlados por LOS PROPIETARIOS, y por tanto la recolección, tratamiento y protección de dichos datos se rige por las políticas de privacidad y protección de datos establecidas por la Constitución Política de Colombia y el régimen legal vigente y aplicable sobre la protección de datos personales.
+
+El usuario reconoce que el ingreso de su información personal la realiza de manera libre y voluntaria, quienes optan por acceder a esta aplicación en Colombia o desde otro territorio o país, lo hacen de forma voluntaria y son responsables del cumplimiento de las leyes locales, en la medida en que dichas leyes sean aplicables en su correspondiente país.
+
+El usuario acepta que la fotografía suministrada en su perfil así como su apodo y descripción será visible para los demás usuarios en los rankings que se generen por su participación en los juegos. En caso tal que el usuario no desee proveer un apodo, el ranking mostrará su nombre y apellido completo suministrado al momento del registro. Si el usuario no desea que su foto sea visible para los demás usuarios, este no se encuentra obligado a colocar una.
+
+El acceso por parte de menores de edad debe ser supervisado por una persona en mayoría de edad en todo momento desde la descarga y en el uso de LA APLICACIÓN. En el evento en que no se cumpla esta condición de acompañamiento y supervisión, por favor no hacer uso de la aplicación.
+
+Los datos personales suministrados de forma voluntaria por el usuario tendrán un tratamiento legítimo y ajustado a la ley y por medio de estos términos y condiciones el usuario autoriza el uso de los mismos para las siguientes finalidades:
+
+· Enviar información referente a los contenidos de LA APLICACIÓN
+
+· Crear el perfil para acceder y participar en los juegos y acceder a los servicios
+
+· Enviar información promocional por parte de LOS PROPIETARIOS
+
+· Compartir los datos con fines promocionales y comerciales con LOS PROPIETARIOS
+
+· Dar a conocer, transferir y/o trasmitir los datos personales suministrados de forma voluntaria por los usuarios de LA APLICACIÓN dentro y fuera del país a terceros a consecuencia de un contrato, ley o vínculo lícito que así lo requiera
+
+· Para la participación en los distintos juegos y concursos desarrollados en LA APLICACIÓN
+
+· Para efectos estadísticos y de comportamiento de LA APLICACIÓN
+
+Para todo lo anterior el usuario por medio de estos términos y condiciones otorga autorización libre, previa, expresa e informada, sus consultas y reclamos, así como cualquier trámite referente al tratamiento de los datos personales debe solicitarlos a través del correo electrónico appmillonarios@2waysports.com
+
+6. INTEGRACIÓN E INTERACCIÓN CON OTRAS APLICACIONES.
+
+Los links de otras aplicaciones o redes sociales tales como Facebook®, Instagram®, twitter® y otras, en LA APLICACIÓN pueden mostrar contenido que no están bajo el control de LOS PROPIETARIOS, no tienen control sobre la naturaleza y el contenido de esos sitios y no está recomendando estos sitios, su contenido, productos o servicios.
+
+No se acepta responsabilidad por el contenido del sitio de un tercero con el cual existe un link de hipertexto y no ofrece garantía (explícita o implícita) ni incide ni responde en cuanto al contenido de la información en esos sitios, ya que no está recomendando estos sitios.
+
+Por ello, el usuario es responsable de verificar los Términos y Condiciones, Política Legal y de Privacidad de los sitios, de un tercero con los cuales se enlaza. Asimismo LOS PROPIETARIOS no asumen ninguna responsabilidad por pérdida directa, indirecta o consecuencial por el uso de un sitio de un tercero.
+
+7. SUSPENSIÓN Y RETIRO DEL ACCESO A LA APLICACIÓN
+
+Si un usuario incumple los presentes Términos y Condiciones, o cualesquiera otras disposiciones incorporadas en LA APLICACIÓN, se podrá suspender y terminar definitivamente el acceso a LA APLICACIÓN. Ante dicho incumplimiento, se aplicarán las sanciones establecidas y el adelantamiento de las acciones judiciales pertinentes.
+
+8. ACEPTACIÓN TOTAL DE LOS PRESENTES TÉRMINOS Y CONDICIONES
+
+El usuario manifiesta de forma libre que acepta expresamente los Términos y Condiciones, siendo condición necesaria, esencial y fundamental para el acceso y uso de LA APLICACIÓN. En el evento en que no esté de acuerdo con estos Términos y Condiciones, por favor abandone de forma inmediata LA APLICACIÓN.
+
+Se podrán modificar los presentes términos y condiciones, avisando a los usuarios en LA APLICACIÓN o mediante redes sociales, SMS y/o correo electrónico de los usuarios, las cuales se entenderán aceptadas por el usuario si sigue usando LA
+
+APLICACIÓN. La Compañía se reserva el derecho de eliminar o prohibir a cualquier Usuario la utilización de esta Aplicación Móvil a su sola discreción.
+
+Estos términos y condiciones y todo lo que tenga que ver con esta aplicación, se rigen por las leyes de colombianas.
+
+9.EXONERACIÓN
+
+Los Usuarios liberarán de toda responsabilidad y exonerarán a LOS PROPIETARIOS de todo reclamo, demanda, responsabilidad civil, causa legal, querella o daños y perjuicios (incluidos los honorarios y los gastos razonables de abogados) que surjan como consecuencia del uso indebido e ilegal que dichos Usuarios hagan de la Aplicación Móvil (incluidos nuestros productos, servicios y Contenido), incluyendo, entre otros, la información, contenido o entrega incorrectos de la Aplicación Móvil, o de los productos y servicios de LOS PROPIETARIOS o de terceros.
+
+10. DIVISIBILIDAD
+
+Si cualquier cláusula de estos Términos y Condiciones resultara inválida, nula o inaplicable, las cláusulas restantes conservarán de todos modos su total validez.
+
+11.PREGUNTAS Y COMENTARIOS
+
+Los Usuarios que tengan alguna pregunta, duda, queja sobre los Términos y Condiciones para Aplicaciones Móviles pueden ponerse en contacto con el webmaster en relación con esta Aplicación Móvil escribiendo al siguiente correo electrónico: • appmillonarios@2waysports.com</p>
+</body>
+</html>
+<?php
+/*
 echo "TYC";
 echo "<pre>";
 print_r($_GET);
 
-echo "</pre>";
+echo "</pre>";*/
 ?>
