a53af061902aa3d0fcb999db9c2f2ffc5b345023
gbanchs <gabanchsra31@hotmail.com>
2017-12-09 20:09:41 -0500

    Cambio index compartir

--- public/compartir/index.php
+++ public/compartir/index.php
@@ -2,18 +2,67 @@
 
 
 
-if($_GET['seccion']=='alineacion' || $_GET['seccion']=='alineación'){
+if($_GET['seccion']=='alineacion' || $_GET['seccion']=='alineación' || $_GET['seccion']=='alineaciónoficial'){
+
+	$url="";
+	$imagen="https://s3.amazonaws.com/cmsmillos/compartir/tabla.jpg";
+	$descripcion="¡ASÍ VA EL EMBAJADOR EN LA LIGA AGUILA!";
+	$titulo="Alineación";
+	
+	
+	}elseif($_GET['seccion']=='tabla' || $_GET['seccion']=='tabla' || $_GET['tabla']=='tabla'){
 	$url="";
 	$imagen="https://s3.amazonaws.com/cmsmillos/compartir/tabla.jpg";
 	$descripcion="¡ASÍ VA EL EMBAJADOR EN LA LIGA AGUILA!";
 	$titulo="Tabla";
 	
 	
-}else{
+	
+	}elseif($_GET['seccion']=='calendario' || $_GET['seccion']=='calendario' || $_GET['seccion']=='calendario'){
+	$url="";
+	$imagen="https://s3.amazonaws.com/cmsmillos/compartir/calendario.jpg";
+	$descripcion="¡NO TE PIERDAS NINGÚN PARTIDO DE MILLONARIOS Y CONOCE TODA LA INFORMACIÓN OFICIAL!";
+	$titulo="Calendario";
+	
+	
+	
+	}elseif($_GET['seccion']=='noticias' || $_GET['seccion']=='noticias' || $_GET['seccion']=='noticias'){
+	$url="";
+	$imagen="https://s3.amazonaws.com/cmsmillos/compartir/noticias.jpeg";
+	$descripcion="¡CONOCE LAS ÚLTIMAS NOTICIAS DEL EMBAJADOR!";
+	$titulo="Noticias";
+	
+    }elseif($_GET['seccion']=='estadisticas' || $_GET['seccion']=='estadisticas' || $_GET['seccion']=='estadisticas'){
+	$url="";
+	$imagen="https://s3.amazonaws.com/cmsmillos/compartir/estadisticas.jpg";
+	$descripcion="¡CONOCE LAS ESTADÍSTICAS DEL BALLET AZUL EN ESTA TEMPORADA!";
+	$titulo="Estadisticas";
+	
+	 }elseif($_GET['seccion']=='equipo' || $_GET['seccion']=='equipo' || $_GET['seccion']=='equipo'){
+	$url="";
+	$imagen="https://s3.amazonaws.com/cmsmillos/compartir/equipo.jpeg";
+	$descripcion="¡CONOCE LA PLANTILLA EMBAJADORA Y APLAUDE A TU JUGADOR FAVORITO!";
+	$titulo="Equipo";
+	
+	 }elseif($_GET['seccion']=='tiendavirtual' || $_GET['seccion']=='tiendavirtual' || $_GET['seccion']=='tiendavirtual'){
+	$url="";
+	$imagen="https://s3.amazonaws.com/cmsmillos/compartir/tiendas.jpg";
+	$descripcion="¡ADQUIERE LOS PRODUCTOS OFICIALES DE TU EQUIPO!";
+	$titulo="Tienda Virtual";
+	
+	 }elseif($_GET['seccion']=='futbolbase' || $_GET['seccion']=='fútbolbase' || $_GET['seccion']=='futbolbase'){
+	$url="";
+	$imagen="https://s3.amazonaws.com/cmsmillos/compartir/futbol_base.jpeg";
+	$descripcion="¡CONOCE A LAS FUTURAS ESTRELLAS DEL CUADRO EMBAJADOR!";
+	$titulo="Fútbol Base";
+	
+	
+	
+	}else{
 	$url="";
 	$imagen="https://s3.amazonaws.com/cmsmillos/compartir/compartaelapp.jpeg";
 	$descripcion="¡COMPARTE LA APP CON TODOS TUS AMIGOS EMBAJADORES Y CONVIÉRTETE EN HINCHA OFICIAL!";
-	$titulo="COMPARTIR LA APP";
+	$titulo="Comparte tu pasión";
 	
 	
 	
@@ -42,17 +91,19 @@ if($_GET['seccion']=='alineacion' || $_GET['seccion']=='alineación'){
 <body>
 	<div class="contenedor">
 		<header>
-			<h1>
-				Ya yo soy Hincha Oficial del Embajador, <br>¿Y tu que esperas? 
-			</h1>
+			<h2><b><?php echo $titulo;?></b></h2>
+			<h3><b><?php echo $descripcion;?></b></h3>
 		</header>
 		<div class="contenido">
 			<!--IMAGEN-->
 			<img src="<?php echo $imagen;?>" class="img-content">
 			<!--TEXTO-->
 			<div class="texto">
-				<h2><b><?php echo $titulo;?></b></h2>
-				<h3><b><?php echo $descripcion;?></b></h3>
+				
+				
+				<h4>
+				Yo ya soy Hincha Oficial del Embajador, <br>¿Y tu que esperas? <br>Descarga el app
+			</h4>
 			</div>
 		</div>
 		<footer>
