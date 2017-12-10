b099d27d44386663eff830295b217b61c35b4f86
reduque <rafael.duque@gmail.com>
2017-12-09 23:59:42 -0400

    Pruebas de email

--- app/Http/Controllers/BorrarController.php
+++ app/Http/Controllers/BorrarController.php
@@ -17,7 +17,7 @@ class BorrarController extends Controller
         ];
 
         Mail::send('emails.prueba', $data, function($message) use ($data) {
-            $message->from('gabanchsra31@hotmail.com');
+            $message->from('app@appmillonariosfc.com');
             $message->to($data['email'], $data['nombre'])->subject('Prueba de email');
         });
 
