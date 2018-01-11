93a55315b6e563499f5511f2b1619cf9042df377
gbanchs <gabanchsra31@hotmail.com>
2018-01-11 10:43:16 -0400

    Agregando timer para que aparezca el landing de descargar antes de direccionar a tiendas

--- resources/views/compartir/referidos/descargar.blade.php
+++ resources/views/compartir/referidos/descargar.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Millonarios FC</title>
-	
+
 	<link rel="stylesheet" href="{{ asset('compartir/css/bootstrap-grid.min.css') }}" />
 	<link rel="stylesheet" href="{{ asset('compartir/css/bootstrap.min.css') }}" />
 	<link rel="stylesheet" href="{{ asset('compartir/css/main.css') }}" />
@@ -12,10 +12,10 @@
 
 <body>
 	<!--CONTENEDOR-->
-	<div class="container-fluid "> 
-		
+	<div class="container-fluid ">
+
 		<!--contenido-->
-		
+
 		<section class="row justify-content-center mt-5 no-gutters"><!-- clase no-gutter-->
 
 			<div class="col-12 col-lg-7 col-xl-3">
@@ -23,7 +23,7 @@
 				<!-- aqui iban los textos, los remplace por una imagen-->
 
 				<img src="{{ asset('compartir/images/text2.png') }}" alt="" class="img-fluid p-4">
-				
+
 				<img src="{{ asset('compartir/images/text3.png') }}" alt="" class="img-fluid p-4">
 
 				<img src="{{ asset('compartir/images/text4.png') }}" alt="" class="img-fluid p-4">
@@ -33,8 +33,8 @@
 			<!-- este logo paso a estar abajo -->
 			<div class="col-12  col-lg-5 col-xl-3">
 				<img src="{{ asset('compartir/images/logo_millos.png') }}" class="col-8" alt="">
-				
-			</div>			
+
+			</div>
 		</section>
 		<section class="row justify-content-around  mb-5 no-gutters">
 			<div class="col-6 col-xl-4 col-lg-4">
@@ -49,13 +49,41 @@
 
 				</div>
 			</section>
-			
-			
+
+
 
 			<!-- fin contenido-->
 
 		</div>
 		<!-- FIN CONTENEDOR-->
+
+	<script type="text/javascript">
+
+
+        function getMobileOperatingSystem() {
+            console.log("executing redirect to the stores...");
+
+            var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+            if (/android/i.test(userAgent)) {
+                window.location.href = 'https://play.google.com/store/apps/details?id=com.Wise.SeleccionColombiaOficial&hl=es';
+                return "Android";
+            } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+                window.location.href = 'https://itunes.apple.com/co/app/seleccion-colombia-oficial/id1210353281?mt=8';
+                return "iOS";
+            } else {
+
+                return false;
+            }
+        }
+
+
+        setTimeout(function () {
+
+            getMobileOperatingSystem()
+        }, 10000);
+
+	</script>
 	</body>
 
 	</html>
