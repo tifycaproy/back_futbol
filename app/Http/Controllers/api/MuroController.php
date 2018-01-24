80f5206fd752c6edcfed1850c577147a931c2fde
reduque <rafael.duque@gmail.com>
2018-01-24 07:32:29 -0400

    Comentarios post

--- app/Http/Controllers/api/MuroController.php
+++ app/Http/Controllers/api/MuroController.php
@@ -189,7 +189,7 @@ class MuroController extends Controller
             if($idpost=='')  return ["status" => "fallo", "error" => ['Idpost incorrecto']];
 
 
-            $comentarios=MuroComentario::orderby('created_at','desc')->paginate(25);
+            $comentarios=MuroComentario::where('muro_id', $idpost)->orderby('created_at','desc')->paginate(25);
             $data["status"]='exito';
             $data["data"]=[];
             foreach ($comentarios as $comentario) {
