79601ee280347aed88473484adc38c9a13cf2a05
yoelismendoza <yoelis.mendoza@laplanamartinez.com>
2018-01-24 11:59:10 -0400

    cambio onceideal

--- resources/views/compartir/onceideal.blade.php
+++ resources/views/compartir/onceideal.blade.php
@@ -61,8 +61,8 @@
                 <div class="col-12 col-lg-6 col-xl-4"><!-- ETIQUETA REMPLAZADA (15/01/2018)-->
                     <div class="texto mt-3 pl-4 pr-4">
                         <!-- Texto-->
-                        <h2>¡No dejemos de seguir nunca al más grande!</h2>
-                        <h2 class="mt-3"><b>DESCARGA LA APP OFICIAL DEL MILLONARIOS FC</b></h2>
+                        <h2>¡NO DEJEMO NUNCA DE SEGUIR A LA MÁS GRANDE!</h2>
+                        <h2 class="mt-3"><b>DESCARGA LA APP OFICIAL DE MILLONARIOS FC</b></h2>
                         
                     </div>
                 </div>
