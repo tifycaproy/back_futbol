53e5806f3694c2db347a2f534999be91e0843de0
Julio Fuentes <jefr90@gmail.com>
2018-02-01 16:07:38 -0400

    Mas pruebas

--- app/Http/Controllers/api/UsuariosController.php
+++ app/Http/Controllers/api/UsuariosController.php
@@ -572,7 +572,7 @@ class UsuariosController extends Controller
 
     public function subirUsuarios()
     {
-        $usuarios = Usuario::first()->get();
+        $usuarios = Usuario::all()->first()->get();
         dd($usuarios);     
     }
 }
