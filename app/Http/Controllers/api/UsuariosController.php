1aaccab63cd493b4f7c7b808517c6dcac6233232
Julio Fuentes <jefr90@gmail.com>
2018-02-02 23:42:09 -0400

    Preparando para staging

--- app/Http/Controllers/api/UsuariosController.php
+++ app/Http/Controllers/api/UsuariosController.php
@@ -572,7 +572,7 @@ class UsuariosController extends Controller
 
     public function subirUsuarios()
     {
-        $usuarios = Usuario::all()->take(10);
+        $usuarios = Usuario::all();
         foreach($usuarios as $usuario){
         $usuario->updated_at = date("Y-m-d H:i:s");
         $usuario->save();
