3765cf0a55080a09e453e747fb2c76897bde11d5
reduque <rafael.duque@gmail.com>
2018-02-09 14:50:47 -0400

    CI en usuarios

--- app/Http/Controllers/api/UsuariosController.php
+++ app/Http/Controllers/api/UsuariosController.php
@@ -422,7 +422,7 @@ class UsuariosController extends Controller
                 return ["status" => "fallo", "error" => $errors];
             }
             //fin validaciones
-            $usuario=Usuario::where('id',$idusuario)->first(['id as idusuario','nombre','apellido','email','apodo','celular','pais','ciudad','fecha_nacimiento','genero','foto','created_at','foto_redes','created_at','referido']);
+            $usuario=Usuario::where('id',$idusuario)->first(['id as idusuario','ci','nombre','apellido','email','apodo','celular','pais','ciudad','fecha_nacimiento','genero','foto','created_at','foto_redes','created_at','referido']);
             $usuario=$usuario->toArray();
             $usuario["fecha_vencimiento"]=date('Y-m-d',strtotime('+1 year',strtotime($usuario['created_at'])));
 
