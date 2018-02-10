1d38410b3572710eba27bf4755777cda416c1a75
gbanchs <gabanchsra31@hotmail.com>
2018-02-10 19:04:33 -0400

    Arreglando conflictos en UserController

--- app/Http/Controllers/api/UsuariosController.php
+++ app/Http/Controllers/api/UsuariosController.php
@@ -120,23 +120,14 @@ class UsuariosController extends Controller
         $request = get_object_vars($request);
         try {
             //Validaciones
-<<<<<<< HEAD
+
             $errors = [];
             if (!isset($request["email"])) $errors[] = "El email es requerido";
             if (!isset($request["nombre"])) $errors[] = "El nombre es requerido";
             if (!isset($request["clave"])) $errors[] = "La clave es requerida";
             if (!isset($request["ci"])) $errors[] = "La cedula es requerida";
             if (count($errors) > 0) {
-=======
-            $errors=[];
-            if(!isset($request["email"])) $errors[]="El email es requerido";
-            if(!isset($request["nombre"])) $errors[]="El nombre es requerido";
-            if(!isset($request["clave"])) $errors[]="La clave es requerida";
-            if(!isset($request["ci"])) $errors[]="La cédula es requerida";
 
-            if(count($errors)>0){
-
->>>>>>> 45432ff86f5e9ea62a6943edf793bfbaeb87697e
                 return ["status" => "fallo", "error" => $errors];
             }
             //fin validaciones
@@ -151,20 +142,12 @@ class UsuariosController extends Controller
             if (Usuario::where('email', $email)->first()) {
                 return ["status" => "fallo", "error" => ["El email ya se encuentra registrado"]];
             }
-<<<<<<< HEAD
-            if (Usuario::where('ci', $ci)->first()) {
-                return ["status" => "fallo", "error" => ["La cedula ya se encuentra registrada"]];
-            }
-            if (isset($request["apodo"])) if ($request["apodo"] <> '') if (Usuario::where('apodo', $request["apodo"])->first()) {
-=======
 
             if(Usuario::where('ci',$ci)->first()){
                 return ["status" => "fallo", "error" => ["La cédula ya se encuentra registrada"]];
             }
 
             if(isset($request["apodo"])) if($request["apodo"]<>'') if(Usuario::where('apodo',$request["apodo"])->first()){
-
->>>>>>> 45432ff86f5e9ea62a6943edf793bfbaeb87697e
                 return ["status" => "fallo", "error" => ["El apodo ya se encuentra registrado"]];
             }
             $request["clave"] = password_hash($request["clave"], PASSWORD_DEFAULT);
