d3813755f9c0b725b6cdf7b3b3e2d93ecb4fc0bf
Julio Fuentes <jefr90@gmail.com>
2018-02-12 14:55:15 -0400

    Implementación de filtro de profanidad al publicar posts o comentarios

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Webup\LaravelSendinBlue\SendinBlueServiceProvider::class,
+        Askedio\Laravel5ProfanityFilter\Providers\ProfanityFilterServiceProvider::class,
 
     ],
 
