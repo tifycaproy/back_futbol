b9a3ec127cd54fcd3309db93f8212601ef173c52
Julio Fuentes <jefr90@gmail.com>
2018-02-16 13:01:53 -0400

    Modificacion de exception

--- app/Exceptions/UserDoradoException.php
+++ app/Exceptions/UserDoradoException.php
@@ -6,5 +6,14 @@ use Exception;
 
 class UserDoradoException extends Exception
 {
-    //
+	public function render($request, Exception $exception)
+	{
+
+		if ($exception instanceof UserDoradoException) {
+			return response()->json(['status' => 'fallo','error'=>["Debe ser hincha dorado para realizar esta acción"]]);
+		}
+
+		return parent::render($request, $exception);
+	}
+
 }
