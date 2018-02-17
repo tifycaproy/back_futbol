6fa9d5da5f98ffacc18ea83035a165737c5ae0d6
Julio Fuentes <jefr90@gmail.com>
2018-02-17 16:36:26 -0400

    Fix

--- app/Exceptions/UserDoradoException.php
+++ app/Exceptions/UserDoradoException.php
@@ -12,7 +12,7 @@ class UserDoradoException extends Exception
 		if ($exception instanceof UserDoradoException) {
 			return response()->json(['status' => 'fallo','error'=>["Debe ser hincha dorado para realizar esta acción"]]);
 		}
-
+		
 		return parent::render($request, $exception);
 	}
 
