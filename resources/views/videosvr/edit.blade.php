68a6cd1cf405702b3dbaea785105cb95b8ac88cc
J-Ibarra <ibarrajj@gmail.com>
2018-02-19 15:39:08 -0400

    Vr Solo Dorado

--- resources/views/videosvr/edit.blade.php
+++ resources/views/videosvr/edit.blade.php
@@ -5,125 +5,150 @@
 @endsection
 
 @section('content')
-<div class="row">
-     <div class="col-lg-6">
-        <h1 class="page-header">Video VR</h1>
-    </div>
-    <div class="col-lg-12">
-        <ol class="breadcrumb">
-            <li><a href="{{ route('home') }}"><i class="fa fa-dashboard"></i> Inicio</a></li>
-            <li><a href="{{ route("videosvr.index") }}"><i class="fa fa-fw fa-pencil"></i> Video VR</a></li>
-            <li>Editar</li>
-        </ol>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-10">
-    @if($notificacion=Session::get('notificacion'))
-        <div class="alert alert-success">{{ $notificacion }}</div>
-    @endif
-    @if($notificacion_error=Session::get('notificacion_error'))
-        <div class="alert alert-danger">{{ $notificacion_error }}</div>
-    @endif
-    </div>
-    <div class="col-lg-2">
-        <p class="text-right"><a href="{{ route('videosvr.index') }}" class="btn btn-sm btn-primary"><i class="fa fa-fw fa-list"></i> Volver a la lista</a></p>
-    </div>
-</div>
-<form role="form" action="{{ route('videosvr.update', codifica($video->id)) }}" method="POST" enctype="multipart/form-data">
-    {{ csrf_field() }}
-    {{ method_field('PUT') }}
     <div class="row">
         <div class="col-lg-6">
-            <div class="form-group{{ $errors->has('titulo') ? ' has-error' : '' }}">
-                <label>Título</label>
-                <input type="text" class="form-control" name="titulo" value="{{ old('titulo', $video->titulo) }}" maxlength="100" required autofocus>
-                @if ($errors->has('titulo'))
-                    <p class="help-block">{{ $errors->first('titulo') }}</p>
-                @endif
-            </div>
+            <h1 class="page-header">Video VR</h1>
         </div>
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label>Video</label>
-                <input type="text" class="form-control" name="video" value="{{ old('video', $video->video) }}" maxlength="200" required>
-            </div>
-        </div>
-    </div>
-    <div class="row">
         <div class="col-lg-12">
-            <div class="form-group">
-                <label>Descripción</label>
-                <textarea name="descripcion" class="form-control">{{ old('descripcion', $video->descripcion) }}</textarea>
+            <ol class="breadcrumb">
+                <li><a href="{{ route('home') }}"><i class="fa fa-dashboard"></i> Inicio</a></li>
+                <li><a href="{{ route("videosvr.index") }}"><i class="fa fa-fw fa-pencil"></i> Video VR</a></li>
+                <li>Editar</li>
+            </ol>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-10">
+            @if($notificacion=Session::get('notificacion'))
+                <div class="alert alert-success">{{ $notificacion }}</div>
+            @endif
+            @if($notificacion_error=Session::get('notificacion_error'))
+                <div class="alert alert-danger">{{ $notificacion_error }}</div>
+            @endif
+        </div>
+        <div class="col-lg-2">
+            <p class="text-right"><a href="{{ route('videosvr.index') }}" class="btn btn-sm btn-primary"><i
+                            class="fa fa-fw fa-list"></i> Volver a la lista</a></p>
+        </div>
+    </div>
+    <form role="form" action="{{ route('videosvr.update', codifica($video->id)) }}" method="POST"
+          enctype="multipart/form-data">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="form-group{{ $errors->has('titulo') ? ' has-error' : '' }}">
+                    <label>Título</label>
+                    <input type="text" class="form-control" name="titulo" value="{{ old('titulo', $video->titulo) }}"
+                           maxlength="100" required autofocus>
+                    @if ($errors->has('titulo'))
+                        <p class="help-block">{{ $errors->first('titulo') }}</p>
+                    @endif
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="form-group">
+                    <label>Video</label>
+                    <input type="text" class="form-control" name="video" value="{{ old('video', $video->video) }}"
+                           maxlength="200" required>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-4">
-            <div class="form-group">
-                <label>Foto</label>
-                <div class="slim">
-                    <input name="foto" type="file" accept="image/jpeg, image/png, image/gif" />
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="form-group">
+                    <label>Descripción</label>
+                    <textarea name="descripcion"
+                              class="form-control">{{ old('descripcion', $video->descripcion) }}</textarea>
                 </div>
-                <label><span>Mínimo 512 x 256 píxeles | JPG, PNG y GIF</span></label>
-                @if($video->foto<>'')
-                <h5>Imagen actual</h5>
-                <p><img src="{{ config('app.url') . 'videosvr/' . $video->foto }}" style="max-width: 100%"></p>
-                @endif
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="form-group">
+                    <label>Solo para usuarios Dorados</label>
+                    <select name="soloUsuariosDorados" class="form-control">
+                        <option value="0" @if(old('soloUsuariosDorados', $video->dorado)=='0') selected @endif>
+                            No
+                        </option>
+                        <option value="1" @if(old('soloUsuariosDorados', $video->dorado)=='1') selected @endif>
+                            Si
+                        </option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="form-group">
+                    <label>Foto</label>
+                    <div class="slim">
+                        <input name="foto" type="file" accept="image/jpeg, image/png, image/gif"/>
+                    </div>
+                    <label><span>Mínimo 512 x 256 píxeles | JPG, PNG y GIF</span></label>
+                    @if($video->foto<>'')
+                        <h5>Imagen actual</h5>
+                        <p><img src="{{ config('app.url') . 'videosvr/' . $video->foto }}" style="max-width: 100%"></p>
+                    @endif
 
-              </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-6">
-            <button type="submit" class="btn btn-success"><i class="fa fa-fw fa-check"></i> Guardar</button>  <a href="{{ route('videosvr.index') }}" class="btn btn-primary"><i class="fa fa-fw fa-list"></i> Volver a la lista</a> <a href="{{ route('videosvr.create') }}" class="btn btn-primary"><i class="fa fa-fw fa-plus-circle"></i> Nuevo</a> <a href="{{ route('videosvr_eliminar', codifica($video->id) ) }}" class="btn btn-danger"><i class="fa fa-fw fa-ban"></i> Eliminar</a>
+        <div class="row">
+            <div class="col-lg-6">
+                <button type="submit" class="btn btn-success"><i class="fa fa-fw fa-check"></i> Guardar</button>
+                <a href="{{ route('videosvr.index') }}" class="btn btn-primary"><i class="fa fa-fw fa-list"></i> Volver
+                    a la lista</a> <a href="{{ route('videosvr.create') }}" class="btn btn-primary"><i
+                            class="fa fa-fw fa-plus-circle"></i> Nuevo</a> <a
+                        href="{{ route('videosvr_eliminar', codifica($video->id) ) }}" class="btn btn-danger"><i
+                            class="fa fa-fw fa-ban"></i> Eliminar</a>
+            </div>
         </div>
-    </div>
-</form>
+    </form>
 
 
 @endsection
 @section('javascript')
-<script type="text/javascript">
-$(document).ready(function(){
-    setTimeout(function(){
-        $(".alert").slideUp(500);
-    },10000)
-})
-</script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            setTimeout(function () {
+                $(".alert").slideUp(500);
+            }, 10000)
+        })
+    </script>
 
-<script src="js/slim.jquery.js"></script>
-<script type="text/javascript">
-$(document).ready(function(){
-   $('.slim').slim({
-      label: 'Arrastra tu imagen ó haz click aquí',
-      ratio: 'free',
-      minSize: {
-        width: 500,
-        height: 250
-      },
-      size: {
-        width: 1024,
-        height: 1024
-      },
-      download: false,
-      labelLoading: 'Cargando imagen...',
-      statusImageTooSmall: 'La imagen es muy pequeña. El tamaño mínimo es $0 píxeles.',
-      statusUnknownResponse: 'Ha ocurrido un error inesperado.',
-      statusUploadSuccess: 'Imagen guardada',
-      statusFileType: 'El formato de imagen no es permitido. Solamente: $0.',
-      statusFileSize: 'El tamaño máximo de imagen es 2MB.',
-      buttonConfirmLabel: 'Aceptar',
-      buttonConfirmTitle: 'Aceptar',
-      buttonCancelLabel: 'Cancelar',
-      buttonCancelLabel: "Cancelar",
-      buttonCancelTitle: "Cancelar",
-      buttonEditTitle: "Editar",
-      buttonRemoveTitle: "Eliminar",
-      buttonRotateTitle: "Rotar",
-      buttonUploadTitle: "Guardar"
-    });
-})
-</script>
+    <script src="js/slim.jquery.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.slim').slim({
+                label: 'Arrastra tu imagen ó haz click aquí',
+                ratio: 'free',
+                minSize: {
+                    width: 500,
+                    height: 250
+                },
+                size: {
+                    width: 1024,
+                    height: 1024
+                },
+                download: false,
+                labelLoading: 'Cargando imagen...',
+                statusImageTooSmall: 'La imagen es muy pequeña. El tamaño mínimo es $0 píxeles.',
+                statusUnknownResponse: 'Ha ocurrido un error inesperado.',
+                statusUploadSuccess: 'Imagen guardada',
+                statusFileType: 'El formato de imagen no es permitido. Solamente: $0.',
+                statusFileSize: 'El tamaño máximo de imagen es 2MB.',
+                buttonConfirmLabel: 'Aceptar',
+                buttonConfirmTitle: 'Aceptar',
+                buttonCancelLabel: 'Cancelar',
+                buttonCancelLabel: "Cancelar",
+                buttonCancelTitle: "Cancelar",
+                buttonEditTitle: "Editar",
+                buttonRemoveTitle: "Eliminar",
+                buttonRotateTitle: "Rotar",
+                buttonUploadTitle: "Guardar"
+            });
+        })
+    </script>
 
 @endsection
