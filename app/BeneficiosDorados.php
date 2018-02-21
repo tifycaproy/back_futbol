9cfbd97ba08c1ba27a17696bcb8b1c37928d25bc
J-Ibarra <ibarrajj@gmail.com>
2018-02-21 12:41:44 -0400

    CMS Config Dorados

---
+++ app/BeneficiosDorados.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BeneficiosDorados extends Model
+{
+    protected $table = 'beneficios_dorados';
+
+    protected $fillable = [
+        'id', 'descripcion', 'url',
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+}
--- app/Http/Controllers/ConfiguracionController.php
+++ app/Http/Controllers/ConfiguracionController.php
@@ -3,90 +3,97 @@
 namespace App\Http\Controllers;
 
 @session_start();
-use Illuminate\Http\Request;
-use Aws\S3\S3Client;
 
-use App\Configuracion;
 use App\Calendario;
+use App\Configuracion;
+use Aws\S3\S3Client;
+use Illuminate\Http\Request;
 
 class ConfiguracionController extends Controller
 {
 
     public function index()
     {
-        $configuracion=Configuracion::first();
-        $partidos=Calendario::where("equipo_1",1)->orwhere('equipo_2',1)->orderby('fecha','desc')->get();
-        return view('configuracion.configuracion')->with('configuracion',$configuracion)->with('partidos',$partidos);
+        $configuracion = Configuracion::first();
+        $partidos = Calendario::where("equipo_1", 1)->orwhere('equipo_2', 1)->orderby('fecha', 'desc')->get();
+        return view('configuracion.configuracion')->with('configuracion', $configuracion)->with('partidos', $partidos);
     }
+
     public function configuracion_actualizar(Request $request)
     {
-        $data=[
-            'calendario_convodados_id'=> $request->calendario_convodados_id,
-            'calendario_aplausos_id'=> $request->calendario_aplausos_id,
-            'calendario_alineacion_id'=> $request->calendario_alineacion_id,
-            'url_tabla'=> $request->url_tabla,
-            'url_simulador'=> $request->url_simulador,
-            'url_juramento'=> $request->url_juramento,
-            'url_livestream'=> $request->url_livestream,
-            'url_tienda'=> $request->url_tienda,
-            'url_estadisticas'=> $request->url_estadisticas,
-            'url_academia'=> $request->url_academia,
-            'tit_1'=> $request->tit_1,
-            'tit_1_1'=> $request->tit_1_1,
-            'tit_1_2'=> $request->tit_1_2,
-            'tit_2'=> $request->tit_2,
-            'tit_3'=> $request->tit_3,
-            'tit_4'=> $request->tit_4,
-            'tit_4_1'=> $request->tit_4_1,
-            'tit_4_2'=> $request->tit_4_2,
-            'tit_5'=> $request->tit_5,
-            'tit_6'=> $request->tit_6,
-            'tit_6_1'=> $request->tit_6_1,
-            'tit_6_1_1'=> $request->tit_6_1_1,
-            'tit_6_1_2'=> $request->tit_6_1_2,
-            'tit_6_2'=> $request->tit_6_2,
-            'tit_6_3'=> $request->tit_6_3,
-            'tit_6_3_1'=> $request->tit_6_3_1,
-            'tit_6_3_2'=> $request->tit_6_3_2,
-            'tit_7'=> $request->tit_7,
-            'tit_7_1'=> $request->tit_7_1,
-            'tit_7_2'=> $request->tit_7_2,
-            'tit_8'=> $request->tit_8,
-            'tit_9'=> $request->tit_9,
-            'tit_10'=> $request->tit_10,
-            'tit_10_1'=> $request->tit_10_1,
-            'tit_10_2'=> $request->tit_10_2,
-            'tit_11'=> $request->tit_11,
-            'tit_11_1'=> $request->tit_11_1,
-            'tit_11_1_1'=> $request->tit_11_1_1,
-            'tit_11_1_2'=> $request->tit_11_1_2,
-            'tit_11_1_3'=> $request->tit_11_1_3,
-            'tit_11_1_4'=> $request->tit_11_1_4,
-            'tit_12'=> $request->tit_12,
-            'tit_13'=> $request->tit_13,
-            'tit_14'=> $request->tit_14,
-            'tit_14_1'=> $request->tit_14_1,
-            'tit_14_2'=> $request->tit_14_2,
-            'tit_14_2_1'=> $request->tit_14_2_1,
-            'tit_14_2_2'=> $request->tit_14_2_2,
-            'tit_14_3'=> $request->tit_14_3,
-            'tit_15'=> $request->tit_15,
-            'tit_16'=> $request->tit_16,
-            'tit_16_1'=> $request->tit_16_1,
-            'tit_16_2'=> $request->tit_16_2,
-            'tit_16_3'=> $request->tit_16_3,
-            'tit_16_3_1'=> $request->tit_16_3_1,
-            'tit_16_3_2'=> $request->tit_16_3_2,
-            'tit_16_3_3'=> $request->tit_16_3_3,
-            'tit_16_3_4'=> $request->tit_16_3_4,
-            'video_referidos'=> $request->video_referidos,
-            'terminos_referidos'=> $request->terminos_referidos,
+        $data = [
+            'calendario_convodados_id' => $request->calendario_convodados_id,
+            'calendario_aplausos_id' => $request->calendario_aplausos_id,
+            'calendario_alineacion_id' => $request->calendario_alineacion_id,
+            'url_tabla' => $request->url_tabla,
+            'url_simulador' => $request->url_simulador,
+            'url_juramento' => $request->url_juramento,
+            'url_livestream' => $request->url_livestream,
+            'url_tienda' => $request->url_tienda,
+            'url_estadisticas' => $request->url_estadisticas,
+            'url_academia' => $request->url_academia,
+            'tit_1' => $request->tit_1,
+            'tit_1_1' => $request->tit_1_1,
+            'tit_1_2' => $request->tit_1_2,
+            'tit_2' => $request->tit_2,
+            'tit_3' => $request->tit_3,
+            'tit_4' => $request->tit_4,
+            'tit_4_1' => $request->tit_4_1,
+            'tit_4_2' => $request->tit_4_2,
+            'tit_5' => $request->tit_5,
+            'tit_6' => $request->tit_6,
+            'tit_6_1' => $request->tit_6_1,
+            'tit_6_1_1' => $request->tit_6_1_1,
+            'tit_6_1_2' => $request->tit_6_1_2,
+            'tit_6_2' => $request->tit_6_2,
+            'tit_6_3' => $request->tit_6_3,
+            'tit_6_3_1' => $request->tit_6_3_1,
+            'tit_6_3_2' => $request->tit_6_3_2,
+            'tit_7' => $request->tit_7,
+            'tit_7_1' => $request->tit_7_1,
+            'tit_7_2' => $request->tit_7_2,
+            'tit_8' => $request->tit_8,
+            'tit_9' => $request->tit_9,
+            'tit_10' => $request->tit_10,
+            'tit_10_1' => $request->tit_10_1,
+            'tit_10_2' => $request->tit_10_2,
+            'tit_11' => $request->tit_11,
+            'tit_11_1' => $request->tit_11_1,
+            'tit_11_1_1' => $request->tit_11_1_1,
+            'tit_11_1_2' => $request->tit_11_1_2,
+            'tit_11_1_3' => $request->tit_11_1_3,
+            'tit_11_1_4' => $request->tit_11_1_4,
+            'tit_12' => $request->tit_12,
+            'tit_13' => $request->tit_13,
+            'tit_14' => $request->tit_14,
+            'tit_14_1' => $request->tit_14_1,
+            'tit_14_2' => $request->tit_14_2,
+            'tit_14_2_1' => $request->tit_14_2_1,
+            'tit_14_2_2' => $request->tit_14_2_2,
+            'tit_14_3' => $request->tit_14_3,
+            'tit_15' => $request->tit_15,
+            'tit_16' => $request->tit_16,
+            'tit_16_1' => $request->tit_16_1,
+            'tit_16_2' => $request->tit_16_2,
+            'tit_16_3' => $request->tit_16_3,
+            'tit_16_3_1' => $request->tit_16_3_1,
+            'tit_16_3_2' => $request->tit_16_3_2,
+            'tit_16_3_3' => $request->tit_16_3_3,
+            'tit_16_3_4' => $request->tit_16_3_4,
+            'video_referidos' => $request->video_referidos,
+            'terminos_referidos' => $request->terminos_referidos,
+
+            'url_imagen_beneficios_dorados' => $request->url_imagen_beneficios_dorados,
+            'footer_formulario_dorados' => $request->footer_formulario_dorados,
+            'texto_bienvenida_dorados' => $request->texto_bienvenida_dorados,
+            'video_de_bienvenida_dorados' => $request->video_de_bienvenida_dorados,
+            'url_tyc_dorados' => $request->url_tyc_dorados,
         ];
-        if($request->patrocinante){
-            $foto=json_decode($request->patrocinante);
-            $extensio=$foto->output->type=='image/png' ? '.png' : '.jpg';
-            $fileName_foto = (string)(date("YmdHis")) . (string)(rand(1,9)) . $extensio;
-            $picture=$foto->output->image;
+        if ($request->patrocinante) {
+            $foto = json_decode($request->patrocinante);
+            $extensio = $foto->output->type == 'image/png' ? '.png' : '.jpg';
+            $fileName_foto = (string)(date("YmdHis")) . (string)(rand(1, 9)) . $extensio;
+            $picture = $foto->output->image;
             $filepath = 'patrocinantes/' . $fileName_foto;
 
             $s3 = S3Client::factory(config('app.s3'));
@@ -97,10 +104,10 @@ class ConfiguracionController extends Controller
                 'ContentType' => 'image',
                 'ACL' => 'public-read',
             ));
-            $data['patrocinante']=$fileName_foto;
+            $data['patrocinante'] = $fileName_foto;
         }
 
         Configuracion::find(1)->update($data);
-        return redirect()->route('configuracion')->with("notificacion","Se ha guardado correctamente su información");
+        return redirect()->route('configuracion')->with("notificacion", "Se ha guardado correctamente su información");
     }
 }
