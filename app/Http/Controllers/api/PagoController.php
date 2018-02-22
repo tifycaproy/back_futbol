d12144085c1ebb2b8b1bc2279fe9b9b9d00a6bcb
Julio Fuentes <jefr90@gmail.com>
2018-02-22 12:00:02 -0400

    Sigue configuración de pagos

--- app/Http/Controllers/api/PagoController.php
+++ app/Http/Controllers/api/PagoController.php
@@ -11,12 +11,13 @@ class PagoController extends Controller
 
 	public function showPayu($tokenUsuario,$idMembresia)
     {
+    	$datos = new \stdClass();
     	$datos->ApiKey = '4Vj8eK4rloUd272L48hsrarnUA';
     	$datos->merchantId = '508029';
     	$datos->reference = 'TestPayUMillonarios';
     	$datos->Amount = '20000';
     	$datos->currency = 'COP';
-    	$datos->signature = md5($datos->$ApiKey .'~'. $datos->$merchantId.'~'. $datos->$reference . '~' . $datos->$Amount . '~' . $datos->$currency);
+    	$datos->signature = md5($datos->ApiKey .'~'. $datos->merchantId.'~'. $datos->reference . '~' . $datos->Amount . '~' . $datos->currency);
 
     	 return view('pagos.payu.formulario')->with('datos',$datos);
 
--- resources/views/pagos/payu/formulario.blade.php
+++ resources/views/pagos/payu/formulario.blade.php
@@ -4,17 +4,18 @@
 </head>
 
 <body>
+<p>Monto : {{$datos->Amount}} </p>
  <form method="post" action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/">
-  <input name="merchantId"    type="hidden"  value="508029"   >
+  <input name="merchantId"    type="hidden"  value="{{$datos->merchantId}}"   >
   <input name="accountId"     type="hidden"  value="512321" >
-  <input name="ApiKey"     type="hidden"  value="4Vj8eK4rloUd272L48hsrarnUA" >
+  <input name="ApiKey"     type="hidden"  value="{{$datos->ApiKey}}" >
   <input name="description"   type="hidden"  value="Test PAYU"  >
-  <input name="referenceCode" type="hidden"  value="TestPayUMillonarios" >
-  <input name="amount"        type="hidden"  value="20000"   >
+  <input name="referenceCode" type="hidden"  value="{{$datos->reference}}" >
+  <input name="amount"        type="hidden"  value="{{$datos->Amount}}"   >
   <input name="tax"           type="hidden"  value="3193"  >
   <input name="taxReturnBase" type="hidden"  value="16806" >
-  <input name="currency"      type="hidden"  value="COP" >
-  <input name="signature"     type="hidden"  value="3b582a4834312ffea7ce3a9436a135e6"  >
+  <input name="currency"      type="hidden"  value="{{$datos->currency}}" >
+  <input name="signature"     type="hidden"  value="{{$datos->signature}}"  >
   <input name="test"          type="hidden"  value="1" >
   <input name="buyerEmail"    type="hidden"  value="prueba@pagomillonarios.com" >
   <input name="responseUrl"    type="hidden"  value="http://millos-dev.2waysports.com/pagos/payu/response" >
