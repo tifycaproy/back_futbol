cc12c7e7610368a1d4a7a2eba66bd22fc7e5496f
Aquiles Brito <abrito393@gmail.com>
2018-03-12 15:00:50 -0400

    modificado:    app/Http/Middleware/UserDoradoMiddleware.php

--- app/Http/Middleware/UserDoradoMiddleware.php
+++ app/Http/Middleware/UserDoradoMiddleware.php
@@ -19,18 +19,22 @@ class UserDoradoMiddleware
      */
     public function handle($request, Closure $next, $tipo, $nombre)
      {
-        $request1=json_decode($request->getContent());
-        $request1=get_object_vars($request1);
         
-        if(!isset($request1["token"])) {
-            return $next($request);
+        if($request["tipo_post"] != 'video') {
+            $request1=json_decode($request->getContent());
+            $request1=get_object_vars($request1);
+
+            if(!isset($request1["token"])) {
+                return $next($request);
+            }
+        }else{
+            $request1["token"] = $request["token"];
         }
 
         $token = $request1["token"];
         $token = decodifica_token($token);
 
         $usuario = Usuario::where('id',$token)->first();
-
         if($tipo == 'seccion')
         {
             $seccion = SeccionesDoradas::where('nombre',$nombre)->first();
