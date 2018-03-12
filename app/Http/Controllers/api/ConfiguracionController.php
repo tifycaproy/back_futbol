a30e148e615a6200fdef400ca792fb34bf85bd6e
Aquiles Brito <abrito393@gmail.com>
2018-03-12 11:56:16 -0400

    modificado:    app/Http/Controllers/api/ConfiguracionController.php

--- app/Http/Controllers/api/ConfiguracionController.php
+++ app/Http/Controllers/api/ConfiguracionController.php
@@ -16,6 +16,7 @@ class ConfiguracionController extends Controller
      */
     public function index()
     {
+
         $configuracion = Configuracion::first([
             'url_tabla', 'url_simulador', 'url_juramento', 'url_livestream', 'url_tienda', 'url_estadisticas', 'url_academia',
             'tit_1', 'tit_1_1', 'tit_1_2', 'tit_2', 'tit_3', 'tit_4', 'tit_4_1', 'tit_4_2', 'tit_5', 'tit_6', 'tit_6_1', 'tit_6_1_1', 'tit_6_1_2', 'tit_6_2', 'tit_6_3', 'tit_6_3_1', 'tit_6_3_2', 'tit_7',
@@ -23,13 +24,12 @@ class ConfiguracionController extends Controller
             'tit_12', 'tit_13', 'tit_14', 'tit_14_1', 'tit_14_2', 'tit_14_2_1', 'tit_14_2_2', 'tit_14_3', 'tit_15','tit_16', 'tit_16_1', 'tit_16_2', 'tit_16_3', 'tit_16_3_1', 'tit_16_3_2', 'tit_16_3_3', 'tit_16_3_4',
             'patrocinante', 'video_referidos', 'terminos_referidos',
             'url_imagen_beneficios_dorados', 'footer_formulario_dorados', 'texto_bienvenida_dorados',
-            'video_de_bienvenida_dorados', 'url_tyc_dorados','url_popup_dorado','act_pop_inicial','link_pop_inicial'
+            'video_de_bienvenida_dorados', 'url_tyc_dorados','url_popup_dorado','act_pop_inicial','link_pop_inicial','url_popup_inicial'
         ]);
-
         $data["status"] = 'exito';
         $data["data"] = $configuracion;
         $data["data"]["patrocinante"] = config('app.url') . 'patrocinantes/' . $configuracion->patrocinante;
-        $data["data"]["pop_inicial"] = config('app.url') . 'configuracion/' . $configuracion->url_popup_inicial;
+        $data["data"]["pop_inicial"] = config('app.url') . 'configuracion/'.$configuracion->url_popup_inicial;
         $data["data"]["url_vistas"] = config('app.share_url');
         $data["data"]["total_hinchas"] = Usuario::count();
 
