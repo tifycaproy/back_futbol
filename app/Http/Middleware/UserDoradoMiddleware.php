7d5d99993fddb0802c5b8f0218b2e62cec80f195
Aquiles Brito <abrito393@gmail.com>
2018-03-13 12:57:40 -0400

    nuevo archivo: app/Http/Controllers/PuntoReferenciaController.php 	modificado:    app/Http/Controllers/api/MuroController.php 	modificado:    app/Http/Middleware/UserDoradoMiddleware.php 	nuevo archivo: app/PuntoReferencia.php 	nuevo archivo: app/PuntoReferenciaImagen.php 	nuevo archivo: database/migrations/v3/2018_03_13_105604_punto_referencia.php 	nuevo archivo: database/migrations/v3/2018_03_13_105840_punto_referencia_imagenes.php 	nuevo archivo: resources/views/mapa/PuntoReferencia.blade.php

--- app/Http/Middleware/UserDoradoMiddleware.php
+++ app/Http/Middleware/UserDoradoMiddleware.php
@@ -19,12 +19,12 @@ class UserDoradoMiddleware
      */
     public function handle($request, Closure $next, $tipo, $nombre)
      {
-        
+        ;
         if($request["tipo_post"] != 'video' || !isset($request["tipo_post"])) {
 
             $request1=json_decode($request->getContent());
             $request1=get_object_vars($request1);
-
+            
             if(!isset($request1["token"])) {
                 return $next($request);
             }
