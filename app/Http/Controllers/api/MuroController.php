745d2552395140954c3031754fe255aacf823fb2
Julio Fuentes <jefr90@gmail.com>
2018-03-22 11:04:16 -0400

    Fix en notificacion

--- app/Http/Controllers/api/MuroController.php
+++ app/Http/Controllers/api/MuroController.php
@@ -608,6 +608,21 @@ class MuroController extends Controller
                 'Authorization:key=' .$server_key,
                 'Content-Type:application/json'
             );
+             $fields = array('to'=>$key,
+               'notification'=>array('title'=>$title,'body'=>$message),
+               'data'=>array('seccion'=>$seccion,'id_post'=>$id_post));
+
+            $payload = json_encode($fields);
+
+              $curl_session = curl_init();
+                curl_setopt($curl_session, CURLOPT_URL, $path_to_fcm);
+                curl_setopt($curl_session, CURLOPT_POST, true);
+                curl_setopt($curl_session, CURLOPT_HTTPHEADER, $headers);
+                curl_setopt($curl_session, CURLOPT_RETURNTRANSFER, true);
+               curl_setopt($curl_session, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($curl_session, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
+                curl_setopt($curl_session, CURLOPT_POSTFIELDS, $payload);
+                $result = curl_exec($curl_session);
 
     }
 
