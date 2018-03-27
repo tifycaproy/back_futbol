67b5259b18604988ade40125dcc26005ae924cbd
Francis Medina <fmedina@rsegrupovzla.com>
2018-03-27 16:41:54 -0400

    cambios

--- resources/views/compartir/partido.blade.php
+++ resources/views/compartir/partido.blade.php
@@ -42,7 +42,7 @@
                             <img src="{{ $data['bandera_1'] }}" alt="" class="escudos">
                             <h4>{{ $data['equipo_1'] }}</h4>
                         </div>
-                        <div class="col-lg-2">
+                        <div class="col-lg-2" style="margin-top: 5%">
                             @if($data['estado'] =='Finalizado')
                             <h1>{{ $data['goles_1'] }} - {{ $data['goles_2'] }}</h1>
                             @endif
@@ -56,7 +56,7 @@
                             <h4>{{ $data['equipo_2'] }}</h4>
                         </div>
 
-                        <div class="col-12">
+                        <div class="col-12" style="margin-top: 5%">
                             <h6>{{ $data['copa'] }}</h6>
                         </div>
                         <div class="col-12">
