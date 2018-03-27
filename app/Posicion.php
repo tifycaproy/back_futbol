c5319ab20aeefa4e3714de60f88c585afa3cdc62
Francis Medina <fmedina@rsegrupovzla.com>
2018-03-27 13:32:36 -0400

    posiciones

---
+++ app/Posicion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Posicion extends Model
+{
+    protected $table = 'posicions';
+    protected $guarded = ['id'];
+}
---
+++ database/migrations/v3/2018_03_27_105430_create_posicions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePosicionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posicions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pos');
+            $table->integer('equipo_id')->unsigned()->nullable();
+            $table->foreign('equipo_id')->references('id')->on('equipos')->onUpdate('cascade')->onDelete('set null');
+            $table->integer('pt');
+            $table->integer('pj');
+            $table->integer('pg');
+            $table->integer('pe');
+            $table->integer('pp');
+            $table->integer('gf');
+            $table->integer('gc');
+            $table->integer('dif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posicions');
+    }
+}
