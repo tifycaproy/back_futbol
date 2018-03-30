7dedc90d34853f0f57f7be37309e8c833b550c93
Carlos Palacios <carlosspf24@gmail.com>
2018-03-30 14:29:32 -0400

    se crearon unas banderas en el Script y se arreglo la linea ref. a php-fpm

--- .ebextensions/setup/hooks/appdeploy/pre/99_custom_logging.sh
+++ .ebextensions/setup/hooks/appdeploy/pre/99_custom_logging.sh
@@ -6,12 +6,14 @@ then
     /opt/elasticbeanstalk/bin/log-conf -n'nginx' -l'/var/log/nginx/*' -f /opt/elasticbeanstalk/support/conf/nginx.logrotate.conf
     rm -f /etc/logrotate.d/nginx
 fi
+echo "Se creo el archivo log Rotate de nginx"
 
 if [ -f /etc/logrotate.d/php-fpm-7.1 ]
 then
-    /opt/elasticbeanstalk/bin/log-conf -n'php-fpm' -l'/var/log/php-fpm/7.1/*log' -f /opt/elasticbeanstalk/support/conf/php-fpm.logrotate.conf
+    /opt/elasticbeanstalk/bin/log-conf -n'php-fpm' -l'/var/log/php-fpm/*log' -f /opt/elasticbeanstalk/support/conf/php-fpm.logrotate.conf
     rm -f /etc/logrotate.d/php-fpm-7.1
 fi
+echo "Se creo el archivo log rotate de php-fpm"
 
 if [ -d /etc/healthd -a ! -d /var/log/nginx/healthd ]
 then
