fc91768522c29d798f448ea77fe0828171431705
Francis Medina <fmedina@rsegrupovzla.com>
2018-04-03 11:11:44 -0400

    checkDuplicateUsers
    php artisan 2way:checkDuplicateUsers

---
+++ app/Console/Commands/UsuariosDuplicados.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use App\Usuario;
+
+class UsuariosDuplicados extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = '2way:checkDuplicateUsers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Chequeo de Usuarios duplicados por email';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $users = Usuario::select(DB::raw('count(*) as count, email'))->groupBy('email')->havingRaw('count(*) > 1')->get();
+        foreach ($users as $user){
+            $this->info($user);
+        }
+    }
+}
