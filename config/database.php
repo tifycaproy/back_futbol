475d4ad1d1cc8e4e18f60d0509a7afe278866641
J-Ibarra <ibarrajj@gmail.com>
2018-04-04 14:22:24 -0400

    Funtional User Cest

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-if (defined('DB_HOST')) {
+/*if (defined('DB_HOST')) {
     define('DB_HOST', $_SERVER['DB_HOST']);
     define('DB_USERNAME', $_SERVER['DB_USERNAME']);
     define('DB_PASSWORD', $_SERVER['DB_PASSWORD']);
@@ -11,7 +11,7 @@ if (defined('DB_HOST')) {
     define('DB_USERNAME', env('DB_USERNAME', 'forge'));
     define('DB_PASSWORD', env('DB_PASSWORD', ''));
     define('DB_NAME', env('DB_DATABASE', 'forge'));
-}
+}*/
 return [
 
     /*
@@ -55,10 +55,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => DB_HOST,
-            'database' => DB_NAME,
-            'username' => DB_USERNAME,
-            'password' => DB_PASSWORD,
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
