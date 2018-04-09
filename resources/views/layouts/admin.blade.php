5d2edfaa842d8d913341ae41f9c3039c503d1f7c
Julio Fuentes <jefr90@gmail.com>
2018-04-09 12:47:02 -0400

    Fix en layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -127,8 +127,8 @@
                         <a href="{{ route("puntoreferencia.index") }}"><i class="fa fa-fw fa-pencil"></i> Puntos de referencia</a>
                     </li>
                     <li>
-                        -                        <a href="{{ route("multimedia.index") }}"><i class="fa fa-fw fa-pencil"></i> Multimedia</a>
-                    -                    </li>
+                        <a href="{{ route("multimedia.index") }}"><i class="fa fa-fw fa-pencil"></i> Multimedia</a>
+                       </li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
