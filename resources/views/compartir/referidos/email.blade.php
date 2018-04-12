48ef17e7319dcafac7922562e0595377fbd4791d
Julio Fuentes <jefr90@gmail.com>
2018-04-12 17:41:51 -0400

    Otro fix en landing referidos/email

--- resources/views/compartir/referidos/email.blade.php
+++ resources/views/compartir/referidos/email.blade.php
@@ -194,7 +194,7 @@ integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLFo
         }
         var dispositivo = getMobileOperatingSystem();
         $("#form1").submit(function (event) {
-          $(this).find("button[type='submit']").prop('disabled',true);
+          $(this).find("input[type='submit']").prop('disabled',true);
             event.preventDefault();
             var telefono = $('#pais').val() +" "+ $('#celular').val();
             var data_referido = new Object();
@@ -224,7 +224,7 @@ integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLFo
                       document.location = ira;
                     } else {
                       alert(data.error[0]);
-                      $(this).find("button[type='submit']").prop('disabled',false);
+                      $(this).find("input[type='submit']").prop('disabled',false);
                     }
                 },
                 error: function (xhr, status) {
