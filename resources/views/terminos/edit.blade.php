4b698a4c27abd9a67aae7b44c1cf4e440a07cb16
Julio Fuentes <jefr90@gmail.com>
2018-04-13 10:14:26 -0400

    Añadiendo vistas faltantes

---
+++ resources/views/terminos/edit.blade.php
@@ -0,0 +1,156 @@
+@extends('layouts.admin')
+
+@section('css')
+    <link href="css/slim.min.css" rel="stylesheet">
+@endsection
+
+@section('content')
+
+<div class="row">
+     <div class="col-lg-6">
+        <h1 class="page-header">Términos y Condiciones</h1>
+    </div>
+    <div class="col-lg-12">
+        <ol class="breadcrumb">
+            <li><a href="{{ route('home') }}"><i class="fa fa-dashboard"></i> Inicio</a></li>
+            <li><a href="{{ route("terminos.index") }}"><i class="fa fa-fw fa-pencil"></i> Compartir</a></li>
+            <li>Editar ({{ $terminos->titulo }})</li>
+        </ol>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-10">
+    @if($notificacion=Session::get('notificacion'))
+        <div class="alert alert-success">{{ $notificacion }}</div>
+    @endif
+    @if($notificacion_error=Session::get('notificacion_error'))
+        <div class="alert alert-danger">{{ $notificacion_error }}</div>
+    @endif
+    </div>
+    <div class="col-lg-2">
+        <p class="text-right"><a href="{{ route('terminos.index') }}" class="btn btn-sm btn-primary"><i class="fa fa-fw fa-list"></i> Volver a la lista</a></p>
+    </div>
+</div>
+<form role="form" action="{{ route('terminos.update', codifica($terminos->id)) }}" method="POST">
+    {{ csrf_field() }}
+    {{ method_field('PUT') }}
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="form-group">
+                <label>Título</label>
+               <input type="text" class="form-control" name="titulo" value="{{ old('titulo', $terminos->titulo) }}" maxlength="100" required autofocus>
+            </div>
+        </div>
+          
+     
+    </div> 
+
+     <div class="col-lg-8">
+            <div class="form-group">
+                <label>Políticas de privacidad</label>
+                <textarea name="txt1" id="txt1" cols="160" rows="13" class="form-control">{{ old('descripcion', $terminos->txt1) }}</textarea>
+            </div>
+        </div>        
+     
+    </div> 
+ 
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="form-group">
+                <label>Foto</label>
+                <div class="slim">
+                    <input name="archivo" type="file" accept="image/jpeg, image/png, image/gif" />
+                </div>
+                <label><span>Mínimo 512 x 256 píxeles | JPG, PNG y GIF</span></label>
+                @if($terminos->foto<>'')
+                <h5>Imagen actual</h5>
+                <p><img src="{{ config('app.url') . 'terminos/' . $terminos->foto }}" style="max-width: 100%"></p>
+                @endif
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <button type="submit" class="btn btn-success"><i class="fa fa-fw fa-check"></i> Guardar</button>  <a href="{{ route('terminos.index') }}" class="btn btn-primary"><i class="fa fa-fw fa-list"></i> Volver a la lista</a>
+        </div>
+    </div>
+</form>
+
+
+@endsection
+@section('javascript')
+<script type="text/javascript">
+$(document).ready(function(){
+    setTimeout(function(){
+        $(".alert").slideUp(500);
+    },10000)
+})
+</script>
+
+<script src="js/slim.jquery.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+   $('.slim').slim({
+      label: 'Arrastra tu imagen ó haz click aquí',
+      ratio: 'free',
+      minSize: {
+        width: 500,
+        height: 250
+      },
+      size: {
+        width: 1024,
+        height: 1024
+      },
+      download: false,
+      labelLoading: 'Cargando imagen...',
+      statusImageTooSmall: 'La imagen es muy pequeña. El tamaño mínimo es $0 píxeles.',
+      statusUnknownResponse: 'Ha ocurrido un error inesperado.',
+      statusUploadSuccess: 'Imagen guardada',
+      statusFileType: 'El formato de imagen no es permitido. Solamente: $0.',
+      statusFileSize: 'El tamaño máximo de imagen es 2MB.',
+      buttonConfirmLabel: 'Aceptar',
+      buttonConfirmTitle: 'Aceptar',
+      buttonCancelLabel: 'Cancelar',
+      buttonCancelLabel: "Cancelar",
+      buttonCancelTitle: "Cancelar",
+      buttonEditTitle: "Editar",
+      buttonRemoveTitle: "Eliminar",
+      buttonRotateTitle: "Rotar",
+      buttonUploadTitle: "Guardar"
+    });
+})
+</script>
+<script>
+    var editor_config = {
+        path_absolute : "{{ URL::to('/') }}/",
+        selector: "textarea",
+        plugins: [
+            "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            "insertdatetime media nonbreaking save table contextmenu directionality",
+            "emoticons template paste textcolor colorpicker textpattern"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+        relative_urls: false,
+        file_browser_callback : function(field_name, url, type, win) {
+            var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+            var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+            var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+            if (type == 'image') {
+                cmsURL = cmsURL + "&type=Images";
+            } else {
+                cmsURL = cmsURL + "&type=Files";
+            }
+            tinyMCE.activeEditor.windowManager.open({
+                file : cmsURL,
+                title : 'Filemanager',
+                width : x * 0.8,
+                height : y * 0.8,
+                resizable : "yes",
+                close_previous : "no"
+            });
+        }
+    };
+    tinymce.init(editor_config);
+</script>
+@endsection
