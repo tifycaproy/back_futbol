fb14216c3d41dfd18f10b1fd2a27915dabb2b9b0
Julio Fuentes <jefr90@gmail.com>
2018-04-27 14:42:08 -0400

    Nueva variable en configuracion

--- database/migrations/v5/2018_04_27_173056_add_column_avvillas_configuracion_table.php
+++ database/migrations/v5/2018_04_27_173056_add_column_avvillas_configuracion_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnBAvvillasConfiguracionTable extends Migration
+class AddColumnAvvillasConfiguracionTable extends Migration
 {
     /**
      * Run the migrations.
